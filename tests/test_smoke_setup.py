38c8af100ba6e40768c45b010b0af30786ef0f2b
GorkemOrhan <gorkemorhan10@gmail.com>
2026-05-03 19:22:48 +0300

    removed unnecessary files and refactored readme.md

--- tests/test_smoke_setup.py
+++
@@ -1,9 +0,0 @@
-from pages.base_page import BasePage
-
-
-# Verify that the homepage title is not empty after loading.
-def test_hepsiburada_homepage_title_is_not_empty(driver, base_url):
-    base_page = BasePage(driver)
-    base_page.open(base_url)
-
-    assert base_page.get_title().strip() != ""
